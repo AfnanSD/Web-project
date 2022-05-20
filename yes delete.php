51cb098c5994970ad9b27c3919e46c46c9a82e30
alyaj8 <99920164+alyaj8@users.noreply.github.com>
2022-05-21 02:29:08 +0300

    Update yes delete.php

--- yes delete.php
+++ yes delete.php
@@ -5,7 +5,7 @@ $user='root';
 $pass='';
 $dbname="web_project";
 $database=mysqli_connect($host,$user,$pass,$dbname);
- $owner_e5=mysqli_real_escape_string($database,$_SESSION['se']);
+ $owner_e5=mysqli_real_escape_string($database,$_SESSION['Email']);
  $q="DELETE FROM pet_owner WHERE OWNER_EMAIL='$owner_e5' ";
  $result=mysqli_query($database,$q);
 mysqli_close($database);
