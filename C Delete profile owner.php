148863e5904a462f471c4e4c0aae3c6d79cdec58
alyaj8 <99920164+alyaj8@users.noreply.github.com>
2022-05-20 07:26:11 +0300

    Add files via upload

---
+++ C Delete profile owner.php
@@ -0,0 +1,10 @@
+
+<script>
+let proceed = confirm("Are you sure you want to delete account?");
+if(proceed)
+{window.location.href = 'yes delete.php';}
+else
+window.location.href = 'no delete.php';
+</script>
+
+
---
+++ no delete.php
@@ -0,0 +1,1 @@
+<?php header("Location: Costumer page.php");?>
