ac93c18cc64df7ec410163099b5bb87ceb3348ce
Mashaelth <99542700+Mashaelth@users.noreply.github.com>
2022-05-20 21:26:28 +0300

    Update C Add more services.php

--- C Add more services.php
+++ C Add more services.php
@@ -34,7 +34,7 @@ box-shadow:1px 1px;
 </head>
 <body>
     <span style="background-color:white;">
-<table style="margin-top: -9px;margin: left -7px; width: 105%; border-collapse:collapse; background-color:white" >
+<table style="margin-top: -9px; margin-left: -7px; width: 105%; border-collapse:collapse; background-color:white" >
                     <tr>
                         <td width=20.6% height: 30px;><a style="text-decoration: none; color: #44475c;" href="C profile.php">My Account</a></td>
                         <td width=15.6%  height: 30px; background-color: #DCABB3;><a   style="text-decoration: none; color: #44475c;" href="C Add a pet.php">Add a pet</a></td>
