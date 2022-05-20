4a35c22ef837aa6828a47187837ce3003aa1bd05
alyaj8 <99920164+alyaj8@users.noreply.github.com>
2022-05-21 02:32:22 +0300

    Update C Edit profile.php

--- C Edit profile.php
+++ C Edit profile.php
@@ -5,7 +5,7 @@ $user='root';
 $pass='';
 $dbname="web_project";
 $database=mysqli_connect($host,$user,$pass,$dbname);
-$owner_e2=mysqli_real_escape_string($database,$_SESSION['se']);
+$owner_e2=mysqli_real_escape_string($database,$_SESSION['Email']);
 $q = "SELECT * from pet_owner WHERE OWNER_EMAIL='$owner_e2' ";
 
 $result=mysqli_query($database,$q);
@@ -34,7 +34,21 @@ $result=mysqli_query($database,$q);
    <meta name="viewport" content="width=device-width, initial-scale=1">
    <link rel="shortcut icon" type="image/x-icon" href="tinyLogo.PNG" />
  </head>
-
+ <span style="background-color:white;">
+<table style="margin-top: -9px;margin-left: -7px; width: 105%; border-collapse:collapse; background-color:white" >
+                    <tr>
+                        <td width=20.6% height: 30px;><a style="text-decoration: none; color: #44475c;" href="C profile.php">My Account</a></td>
+                        <td width=15.6%  height: 30px; background-color: #DCABB3;><a   style="text-decoration: none; color: #44475c;" href="C Add a pet.php">Add a pet</a></td>
+                        <td width=15.6%  height: 30px; ><a   style="text-decoration: none; color: #44475c;"href="C View pet list.php">Pet List</a></td>
+                        <td width=15.6%  height: 30px; ><a   style="text-decoration: none; color: #44475c;"href="C Add more services.php"> Services</a></td>
+                        <td width=15.6%  height: 30px;><a   style="text-decoration: none; color: #44475c;"href="C Previous appointments.php">View previous appointments</a></td>
+                        <td width=10.6%  height: 30px;><a   style="text-decoration: none; color: #44475c;"href="signout.php" class="logoutb" style="float: right;"><img src="1250678.png" alt="logout icon.png" height="30" width="30"></a></td>
+                        <!--<th>Time</th>
+                        <th>Edit</th>
+                        <th>Cancel</th>-->
+                    </tr>
+        </table>
+</span>
  <body>
     <img src="logo 1.1.jpg" alt="logo" class="aboutUsImage">
      <div>
