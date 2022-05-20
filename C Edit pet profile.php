a4e4ad1b474543a8195c5bf808c62a4b4f79bdbd
Mashaelth <99542700+Mashaelth@users.noreply.github.com>
2022-05-20 21:34:52 +0300

    Update C Edit pet profile.php

--- C Edit pet profile.php
+++ C Edit pet profile.php
@@ -6,7 +6,7 @@ $pass='';
 $dbname="web_project";
 $database=mysqli_connect($host,$user,$pass,$dbname);
 
- $pet_id2=mysqli_real_escape_string($database,$_GET['edit']);
+$pet_id2=mysqli_real_escape_string($database,$_GET['edit']);
  $pet_id=$_GET['edit'];
  
 $q = "SELECT * from pet WHERE PID='$pet_id' ";
@@ -45,6 +45,21 @@ mysqli_close($database);
  </head>
 
 <body>
+<span style="background-color:white;">
+<table style="margin-top: -9px; margin-left :-7px; width: 105%; border-collapse:collapse; background-color:white" >
+                    <tr>
+                        <td width=20.6% height: 30px;><a style="text-decoration: none; color: #44475c;" href="C profile.php">My Account</a></td>
+                        <td width=15.6%  height: 30px; background-color: #DCABB3;><a   style="text-decoration: none; color: #44475c;" href="C Add a pet.php">Add a pet</a></td>
+                        <td width=15.6%  height: 30px; ><a   style="text-decoration: none; color: #44475c;"href="C View pet list.php">Pet List</a></td>
+                        <td width=15.6%  height: 30px; ><a   style="text-decoration: none; color: #44475c;"href="#"> Services</a></td>
+                        <td width=15.6%  height: 30px;><a   style="text-decoration: none; color: #44475c;"href="C Previous appointments.html">View previous appointments</a></td>
+                        <td width=10.6%  height: 30px;><a   style="text-decoration: none; color: #44475c;"href="signout.php" class="logoutb" style="float: right;"><img src="1250678.png" alt="logout icon" height="30" width="30"></a></td>
+                        <!--<th>Time</th>
+                        <th>Edit</th>
+                        <th>Cancel</th>-->
+                    </tr>
+        </table>
+</span>
 <div>
 <h1>Edit pet's profile:</h1>
 		<fieldset>
