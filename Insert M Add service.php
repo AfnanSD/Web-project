621ab5917b74393a28cb99e141cb17f9926594fc
alyaj8 <99920164+alyaj8@users.noreply.github.com>
2022-05-20 12:04:50 +0300

    Update Insert M Add service.php

--- Insert M Add service.php
+++ Insert M Add service.php
@@ -2,22 +2,23 @@
 $host='localhost';
 $user='root';
 $pass='';
-$dbname="web_project2";
+$dbname="web_project";
 $database=mysqli_connect($host,$user,$pass,$dbname);
 
 if(isset($_POST['addsboutton']))
 { 
    $NAME=$_POST["nameofservice"];
-    $S_PHOTO=$_FILES["photoofservice"]['name'];
+    $S_PHOTO=$_POST["photoofservice"];
    $PRICE=$_POST["costofservice"];
    $S_DE=$_POST["S_DE"];
 
-$q="INSERT INTO clinic_service(SERVICE_NAME1, SERVICE_PHOTO, SERVICE_PRICE,SERVICE_DESCRIPTION) VALUES('$NAME','$S_PHOTO',$PRICE, '$S_DE')";
-$result=mysqli_query($database,$q);}
+$q="INSERT INTO clinic_service(SERVICE_NAME,SERVICE_PHOTO, SERVICE_PRICE,SERVICE_DESCRIPTION) VALUES('$NAME','$S_PHOTO',$PRICE, '$S_DE')";
+$result=mysqli_query($database,$q);
 if($result){
     header("Location: M Manger page.php");
     exit(0);
-    }
+    }}
 mysqli_close($database);
 
 ?>
+<?php ?>
