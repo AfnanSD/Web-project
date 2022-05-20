13cfacd9bca4a2a7b5a6d4f974517c5700bae864
alyaj8 <99920164+alyaj8@users.noreply.github.com>
2022-05-20 11:29:30 +0300

    Update update M Edit about us.php

--- update M Edit about us.php
+++ update M Edit about us.php
@@ -3,7 +3,7 @@
 $host='localhost';
 $user='root';
 $pass='';
-$dbname="web_project2";
+$dbname="web_project";
 $database=mysqli_connect($host,$user,$pass,$dbname);
  
   if(isset($_POST['update_button']))
@@ -13,10 +13,11 @@ $database=mysqli_connect($host,$user,$pass,$dbname);
     $ph=$_POST["PHONE_CLINIC"];
     $em=$_POST["EMAIL_CLINIC"];
     $cp=$_FILES["PIC_CLINIC"]['name'];
-    $q1="DELETE FROM CLINIC_PICTURE ";
+
+    $q1="DELETE FROM clinic_pictures ";
     $result=mysqli_query($database,$q1);
 
-     $q2="INSERT INTO CLINIC_PICTURE  (CLINIC_PICTURE2) VALUES('$cp')";
+     $q2="INSERT INTO clinic_pictures  (PICTURES) VALUES('$cp')";
     $result=mysqli_query($database,$q2);}
 
    $q="UPDATE clinic_manager SET CLINIC_EMAIL='$em',CLINIC_PHONE_NUMBER=$ph,CLINIC_DESCRIPTION='$de',CLINIC_LOCATION='$loc' ";
@@ -30,3 +31,4 @@ header("Location: M Manger page.php");
 exit(0);
 }
 ?>
+
