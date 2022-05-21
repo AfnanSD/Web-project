a21b235a1a8cd2d9257ef758efc2441883e43dce
Mashaelth <99542700+Mashaelth@users.noreply.github.com>
2022-05-21 12:00:10 +0300

    Update M Add Service.php

--- M Add Service.php
+++ M Add Service.php
@@ -1,3 +1,10 @@
+<?php  
+    session_start();
+    if(!isset($_SESSION['Email'])){
+        header("Location: Log in page.php?error=Please Sign In again!");
+    }
+
+?> 
 <!DOCTYPE html>
 <html>
 	<head>
